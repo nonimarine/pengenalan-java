f9a0583b81f78e148b7f780f9bbf0d9b1aea6700
Noni Nindya <34081129+nonimarine@users.noreply.github.com>
2020-10-29 00:08:26 +0700

    Add files via upload

---
+++ cilsy/src/main/java/com/cilsy/App.java
@@ -0,0 +1,26 @@
+package com.cilsy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+	
+ public static void main( String[] args )
+ {
+	 
+ System.out.println( getMyName() );
+ }
+ 
+ public static String getMyName() {
+	 
+ return "Hello Welcome To The Club";
+ 
+ public void testApp()
+ {
+	 assertTrue(true);
+	 assertEquals(App.getMyName(), Hello Welcome To The Club");
+ }
+ 
+ }
